10c6e299b2f2c51c49cef36bf075acf5a205dffb
Adi Hendra <adihendra1303@gmail.com>
2024-05-05 22:27:18 +0700

    login

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $userData = [
+            [
+                'username'=> 'tutik',
+                'email'=> 'tutik@gmail.com',
+                'password'=> bcrypt('4567'),
+                'role'=> 'admin'
+            ],
+            [
+                'username'=> 'rinin',
+                'email'=> 'rinin@gmail.com',
+                'password'=> bcrypt('12345'),
+                'role'=> 'admin'
+            ],
+            [
+                'username'=> 'riska',
+                'email'=> 'riska@gmail.com',
+                'password'=> bcrypt('8889'),
+                'role'=> 'customer'
+            ],
+            [
+                'username'=> 'nopa',
+                'email'=> 'nopa@gmail.com',
+                'password'=> bcrypt('0987'),
+                'role'=> 'customer'
+            ],
+            [
+                'username'=> 'rudi',
+                'email'=> 'nika@gmail.com',
+                'password'=> bcrypt('123456'),
+                'role'=>'admin'
+            ],
+            [
+                'username'=> 'kukus',
+                'email'=> 'kukus@gmail.com',
+                'password'=> bcrypt('23456'),
+                'role'=>'customer'
+            ],
+        ];
+
+        foreach($userData as $key => $val){
+            User::create($val);
+        }
+    }
+}
